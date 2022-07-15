14aa4a78ba804144a6228e35649fa554614d9e34
Sergey <duxxless53@ya.ru>
2022-07-16 01:03:40 +0300

    Added the user biometric authentication.

--- ToDo/Supporting files/SceneDelegate.swift
+++ ToDo/Supporting files/SceneDelegate.swift
@@ -1,6 +1,7 @@
 
 
 import UIKit
+import LocalAuthentication
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
@@ -8,6 +9,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         self.window = setWindow(scene)
+        blurEffect()
     }
     
     private func setWindow(_ scene: UIWindowScene) -> UIWindow {
@@ -17,7 +19,43 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         navigationController.viewControllers = [todoTableVC]
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
+        checkUser()
         return window
+    }
+    
+    func sceneWillResignActive(_ scene: UIScene) {
+        if window?.viewWithTag(10) == nil {
+            blurEffect()
+        }
+    }
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        checkUser()
+    }
+    
+    func checkUser() {
+        let context = LAContext()
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) {
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Please authenticate to proceed.") { (success, error) in
+                if success {
+                    DispatchQueue.main.async {
+                        self.window!.viewWithTag(10)?.removeFromSuperview()
+                    }
+                } else {
+                    guard let error = error else { return }
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    func blurEffect() {
+        let blurEffect = UIBlurEffect(style: .light)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.frame = self.window!.frame
+        blurView.tag = 10
+        self.window?.addSubview(blurView)
     }
 }
 
